18a8dd18b26d9dcb86c01398d2345070e7f1dbfc
xmacek27 <matej.macek@email.cz>
2023-09-01 18:46:30 +0200

    Add Image loader

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,11 @@
+import os
+import glob
+
+# creates list of image paths from folder
+def load_images_from_folder(folder_path):
+    image_paths = glob.glob(os.path.join(folder_path, "*.jpg"))
+    return image_paths
+
 def load_image(image_path):
     pass
 
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
 from facade import ContaminationMeasurementFacade
 from strategy import ThresholdingStrategy
 from observer import AnalysisObserver, VisualizationObserver
+from image_processing import load_images_from_folder
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    #use this directory and the folder images 
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    folder_path = os.path.join(current_directory, "images")
+    
+    # Load images
+    image_paths = load_images_from_folder(folder_path)
+    
     thresholding_strategy = ThresholdingStrategy()
     facade = ContaminationMeasurementFacade(thresholding_strategy)
 
@@ -12,5 +22,8 @@ if __name__ == "__main__":
     facade.attach_observer(analysis_observer)
     facade.attach_observer(visualization_observer)
 
-    image_path = "path_to_your_image.jpg"
-    facade.measure_contamination(image_path)
+    for image_path in image_paths:
+        print("Tu")
+        # show the image using plot 
+        plt.imshow(image_path)
+        facade.measure_contamination(image_path)
