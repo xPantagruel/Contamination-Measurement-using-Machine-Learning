af90a10e5cacf61aefeb993bd586c6a61592ce44
xmacek27 <matej.macek@email.cz>
2023-09-01 20:02:12 +0200

    Add image showing

--- image_processing.py
+++ image_processing.py
@@ -3,7 +3,15 @@ import glob
 
 # creates list of image paths from folder
 def load_images_from_folder(folder_path):
-    image_paths = glob.glob(os.path.join(folder_path, "*.jpg"))
+    # Define a list to store image file paths
+    image_paths = []
+    # Use the 'glob' function to search for image files in the directory and its subdirectories
+    # You can specify different image file extensions like '*.jpg', '*.png', '*.jpeg', etc.
+    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', "*.tif"]  # Add more extensions as needed
+
+    for extension in image_extensions:
+        image_paths.extend(glob.glob(os.path.join(folder_path, '**', extension), recursive=True))
+    
     return image_paths
 
 def load_image(image_path):
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from strategy import ThresholdingStrategy
 from observer import AnalysisObserver, VisualizationObserver
 from image_processing import load_images_from_folder
 import matplotlib.pyplot as plt
+from PIL import Image
 
 if __name__ == "__main__":
     #use this directory and the folder images 
     current_directory = os.path.dirname(os.path.realpath(__file__))
     folder_path = os.path.join(current_directory, "images")
-    
+        
     # Load images
     image_paths = load_images_from_folder(folder_path)
     
@@ -21,9 +22,32 @@ if __name__ == "__main__":
 
     facade.attach_observer(analysis_observer)
     facade.attach_observer(visualization_observer)
+        
+    print("Starting facade...")
+    print("Facade will now measure contamination for all images in the folder")
+    
+    # Create a Matplotlib figure to display the images
+    fig = plt.figure()
 
+    # Loop through the image paths and display each image in grayscale
     for image_path in image_paths:
-        print("Tu")
-        # show the image using plot 
-        plt.imshow(image_path)
-        facade.measure_contamination(image_path)
+        # Open the image using Pillow
+        image = Image.open(image_path)
+
+        # Convert the image to grayscale (L mode)
+        image = image.convert('L')
+
+        # Display the grayscale image using Matplotlib with the 'gray' colormap
+        plt.imshow(image, cmap='gray')
+
+        # Add a title with the image file name
+        plt.title(image_path)
+
+        # Click to skip to the next image
+        plt.waitforbuttonpress()
+
+        # Clear the current figure to display the next image
+        plt.clf()
+
+    # Close the Matplotlib figure when done
+    plt.close(fig)
